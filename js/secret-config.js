d79711711826aa84fece4854eebb96e2e05281c1
holaradio <161748364+holaradio@users.noreply.github.com>
2025-10-28 16:07:13 +0100

    Updated in

--- js/secret-config.js
+++ js/secret-config.js
@@ -4,13 +4,13 @@ window.SECRET_CONFIG = {
   obfKey: 77,
 
   // obfuscated trigger hash (already used by secret-init.js)
-  triggerHashObf: "K3l8Li4oeHV/f3R/dXR1fy8of3R0K3krLC5/fHx0dHt0f310LHl5KHl/fXx8KywufysvK3opKCh9KC8ofnh1fA==",
+  triggerHashObf: "fn58fSwrLnl5L3l/KC55dCl+KX18eisoK3koKXp5Ln57eH58eXwsei8udHp+f3R/f3R0fnV8eHR6KH18eHx6eA==",
 
   // users: store obfuscated SHA-256 hex strings for username (lowercased) and password
   users: [
     {
-      usernameHashObf: "Ky54dCx7KH1+fil8KXQoKXovLy8pLy57eX95KX97fnl0e3l9Lnl8Ln56KCt+K3wvdX8peyx7fH58K3x1eykuKA==",
-      passwordHashObf: "fy8vdXsueywofCsrLyh0Ly8re3x7L31+dSt1Li55KXgpenl9fHl4Ln0oKHV5eXwvKC51fXgrKX94eix8K358Lw==",
+      usernameHashObf: "dXV+ey99KCgoeSl+fCh6eX0rdHssLH8ofSt4fXl8ensvLC56eHR+LHx6Lnt9fS58KHl/L3R/Lnh9eCsoKX98eA==",
+      passwordHashObf: "eCx+dXh5e3sre316eHR7Lyguey94K398eix9Ly98fit7eiwrL3V7dXQre3p9KS9/K3UpL3t8dHssKXR+e3l/Lw==",
       redirect: "intra/"
     }
     // add more entries if needed
